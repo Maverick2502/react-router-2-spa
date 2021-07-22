372a73fcdb1bea0255df8169d0bf563c8823b06a
Nurullo <khuwaidullaev@gmail.com>
2021-07-22 22:43:28 +0500

    First commit

---
+++ src/pages/QuoteDetail.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function QuoteDetail() {
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+    </Fragment>
+  );
+}
+
+export default QuoteDetail;
