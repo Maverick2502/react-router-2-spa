435d4a9cf1aaa6cb79c78032c074be4391aa3a0b
Nurullo <khuwaidullaev@gmail.com>
2021-07-23 21:56:00 +0500

    Added Nested Comments

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 import { DUMMY_QUOTES } from "./AllQuotes";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -17,6 +17,13 @@ function QuoteDetail() {
     <Fragment>
       {/* <p>{params.quoteId}</p> */}
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
