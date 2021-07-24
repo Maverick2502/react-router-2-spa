623c60bb13621e7e7be1b3768059ea661f013059
Nurullo <khuwaidullaev@gmail.com>
2021-07-24 16:46:20 +0500

    Sending and Getting Quote data

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 function NewQuote() {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
 
     // Redirects back to 'quotes' query
-    history.push("/quotes");
+    // history.push("/quotes");
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 }
 
 export default NewQuote;
--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,23 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import { DUMMY_QUOTES } from "./AllQuotes";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 function QuoteDetail() {
   const params = useParams();
   const match = useRouteMatch();
 
+  const { quoteId } = params;
+
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuotes.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
       {/* <p>{params.quoteId}</p> */}
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuotes.text} author={loadedQuotes.author} />
       {/* <Route path={`/quotes/${params.quoteId}`} exact> */}
       <Route path={`${match.path}`} exact>
         <div className="centered">
